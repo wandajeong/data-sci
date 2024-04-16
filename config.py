92c0e74565087570cb874f3a0ed52da353597a68
wandajeong <167083015+wandajeong@users.noreply.github.com>
2024-04-16 10:06:42 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+class Constants:
+  SERVERS = ["server_1","server_2"]
+  USERS = ["username1","username2"]
+  PASSWORD = "password"
+  DATABASE = ["DB1","DB2"]
+
+  DC_CODES = ["code1","code2","code3","code4","code5"]
+  MODEL_NAMES = ["model1","model2","model3","model4","model5","model6","model7","model8"]
+  MODEL_PATH =["./model/"+ model for model in MODEL_NAMES]
+  TH = [0.03, 0.8, 0.06, 0.6, 0.5, 0.72, 0.7, 0.63]
